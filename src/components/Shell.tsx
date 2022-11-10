7ff5f315db6a791e0ebdfb219d4463ac421a5385
Hayden Young <hayden@hbjy.dev>
2022-11-10 13:34:31 +0000

    feat: add link to volume mount

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -9,9 +9,16 @@ export const Shell: React.FC<PropsWithChildren> = ({ children }) => {
                     <h1 className="text-3xl font-bold">
                         systemd Unit Generator
                     </h1>
-                    <ul>
+                    <ul className="flex gap-6 underline">
                         <li>
-                            <Link href="/">Services & Timer</Link>
+                            <Link href="/" className="font-bold color-blue-500">
+                                Service & Timer
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/mount" className="font-bold color-blue-500">
+                                Volume Mount
+                            </Link>
                         </li>
                     </ul>
                 </div>
